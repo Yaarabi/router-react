dec7f0f1a258ea2026b3d8af858670be174af63e
Yaarabi <yaarabi318@gmail.com>
2025-05-03 21:35:02 +0100

    feat- useLoction to pass data and some features dinamique

--- src/Component/user/user.jsx
+++ src/Component/user/user.jsx
@@ -3,87 +3,59 @@ import {  useState, useEffect, createContext } from 'react'
 import { useLocation } from 'react-router-dom'
 import React from 'react'
 import "./user.css"
-// import List from '../admin/list'
+
 import { useNavigate } from 'react-router-dom'
 
-// import { Id } from '../form/form'
+
 
 const Props = createContext()
 
 const User = () => {
 
 let location = useLocation()
-let add = location.add
+console.log(location)
+let {name} = location.state || {}
 
-let [arr, setArr]=useState([])
-let [guide, setGuide]= useState([])
+
+
 let [user, setUser]=useState([])
-let [show, setShow]=useState(true)
-
-let move =()=>{
-    setShow(false)
-}
 
 const navigate =useNavigate()
 
 const logOut = ()=>{
     navigate("/")
 }
-// const guide = useContext(Id)
-// console.log(guide)
+
 
 useEffect(
     
         ()=>{
         fetch("https://jsonplaceholder.typicode.com/users")
             .then((res)=>{
-            // console.log(res);
+
             return res.json()
-            // console.log(db);
+
     
             
     
         })
         .then((resu)=>{
-            // console.log(resu);
-            
-            setArr(resu)
             console.log(resu)
+            setUser(
+                resu.find(
+                        (ele)=>(ele.email===name)
+            ))
         })
         .catch((err) => console.log(`illa ${err}`))
         
     
-    }, [])
+    }, [name])
 
-    // const user = arr.find(
-    //     (user)=>(user.email===guide)
-        
-    // )
-    const check = ()=>{
-        setUser(
-            arr.find(
-                    (user)=>(user.email===guide)
-        ))
-    }
 
-    // console.log(user)
 
     return (
-        <Props.Provider value={show}>
         <section>
-        {show && (
-        <>
-        <h2>Plz enter your userName here to retcheck</h2>
-        <div className="label">
-            <input type="text"
-                value={guide}
-                onChange={(e)=>setGuide(e.target.value)}
-                placeholder='email'
-            />
-            <button onClick={()=>(check(), move())}>check</button>
-        </div>
-        </>)}
-        {user && !show && (
+        {user ? (
             <>
             <div className="profile">
                 <h2>{user.name}</h2>
@@ -97,17 +69,17 @@ useEffect(
                     <li>{user.email}</li>
                     <li>{user.phone}</li>
                     <li>{user.website}</li>
-                    <li>{add}</li>
+                    <li>{name}</li>
                 </ul>
             </div>
             </>
-        )}
+        ): <h1>Leoding ...</h1>}
             <img onClick={logOut} src="/public/icon/door.png" style={{height:"40px"}} alt="log out icon" />
         </section>
-        </Props.Provider>
+        // </Props.Provider>
     )
 }
 
 export default User
-export {Props}
+// export {Props}
 
